29b295145e0e8fd271b65e2a6789a74dcd5d459c
Maryna Haiduk <maryna.i.haiduk@gmail.com>
2024-10-20 21:07:47 -0400

    Created a CalculateSumOfDigits method (in DigitOperations) that finds the sum of the digits of both {firstNumber} and {secondNumber}, then returns the result as a string.

--- LearnLab/NumberOperations/DigitOperations.cs
+++ LearnLab/NumberOperations/DigitOperations.cs
@@ -5,7 +5,22 @@ namespace LearnLab.NumberOperations
     {
         public static string CalculateSumOfDigits(int firstNumber, int secondNumber)
         {
-            throw new NotImplementedException();
+            // The sum of the digits of both {firstNumber} and {secondNumber} together is:
+
+            int[] digits = firstNumber.ToString()
+                    .Select(x => int.Parse(x.ToString()))
+                    .Concat(secondNumber.ToString()
+                                .Select(x => int.Parse(x.ToString())))
+                    .ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                sum += digits[i];
+            }
+
+            return sum.ToString();
         }
 
         public static string CheckIfNumbersAreDigitAnagrams(int firstNumber, int secondNumber)
