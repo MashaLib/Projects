ee3f219a46d1af2eb704ddfd7708e198200f3f73
Maryna Haiduk <maryna.i.haiduk@gmail.com>
2024-10-22 18:47:42 -0400

    Added TestMethod: FindCommonDigits_ReturnsCommonDigitsOfTwoNumbersAsString()

--- LearnLab/LearnLab.UnitTests/DigitOperationTests.cs
+++ LearnLab/LearnLab.UnitTests/DigitOperationTests.cs
@@ -7,9 +7,18 @@ namespace LearnLab.UnitTests
     public class DigitOperationTests
     {
         [TestMethod]
-        public void MyTestMethod()
+        public void FindCommonDigits_ReturnsCommonDigitsOfTwoNumbersAsString()
         {
+            // Arrange
+            int firstNumber = 1223;
+            int secondNumber = 4232;
+            string expected = "2, 3";
 
+            // Act
+            string result = DigitOperations.FindCommonDigits(firstNumber, secondNumber);
+
+            // Assert
+            Assert.AreEqual(expected, result);
         }
     }
 }
