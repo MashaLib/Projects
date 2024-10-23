42751f1733e1ecf01181e199fc7bd03df1f3df5d
Maryna Haiduk <maryna.i.haiduk@gmail.com>
2024-10-23 13:51:03 -0400

    Added TestMethod: CalculateSumOfDigits_ReturnsStringOfSumOfDigits_ForTwoPositiveNumbers()

--- LearnLab/LearnLab.UnitTests/DigitOperationTests.cs
+++ LearnLab/LearnLab.UnitTests/DigitOperationTests.cs
@@ -6,10 +6,19 @@ namespace LearnLab.UnitTests
     [TestClass]
     public class DigitOperationTests
     {
-        [TestMethod]
-        public void MyTestMethod()
+        [TestMethod]        
+        public void CalculateSumOfDigits_ReturnsStringOfSumOfDigits_ForTwoPositiveNumbers()
         {
+            // Arrange
+            int firstNumber = 123;
+            int secondNumber = 4;
+            string expected = "10";
+            
+            // Act
+            string result = DigitOperations.CalculateSumOfDigits(firstNumber, secondNumber);
 
+            // Assert
+            Assert.AreEqual(expected, result);
         }
     }
 }
