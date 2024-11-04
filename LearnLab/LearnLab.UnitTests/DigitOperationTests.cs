5fe617301abdf35459b590aaa82942e34c6654eb
Maryna Haiduk <maryna.i.haiduk@gmail.com>
2024-11-04 14:11:25 -0500

    Added TestMethod FindCommonDigits_ReturnsResultAsString_ForNoCommonDigits()

--- LearnLab/LearnLab.UnitTests/DigitOperationTests.cs
+++ LearnLab/LearnLab.UnitTests/DigitOperationTests.cs
@@ -20,5 +20,20 @@ namespace LearnLab.UnitTests
             // Assert
             Assert.AreEqual(expected, result);
         }
+
+        [TestMethod]
+        public void FindCommonDigits_ReturnsResultAsString_ForNoCommonDigits()
+        {
+            // Arrange
+            int firstNumber = 123;
+            int secondNumber = 456;
+            string expected = "";
+
+            // Act
+            string result = DigitOperations.FindCommonDigits(firstNumber, secondNumber);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
     }
 }
