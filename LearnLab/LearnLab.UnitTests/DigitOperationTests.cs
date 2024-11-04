efbe71d263b9d3fb53e3f7319bc4abc7f958a2b6
Maryna Haiduk <maryna.i.haiduk@gmail.com>
2024-11-04 13:47:44 -0500

    Added TestMethod CheckIfNumbersAreDigitAnagrams_ForSameNumbers()

--- LearnLab/LearnLab.UnitTests/DigitOperationTests.cs
+++ LearnLab/LearnLab.UnitTests/DigitOperationTests.cs
@@ -7,9 +7,18 @@ namespace LearnLab.UnitTests
     public class DigitOperationTests
     {
         [TestMethod]
-        public void MyTestMethod()
+        public void CheckIfNumbersAreDigitAnagrams_ForSameNumbers()
         {
+            // Arrange
+            int firstNumber = 123;
+            int secondNumber = 123;
+            string expected = "YES";
 
+            // Act
+            string result = DigitOperations.CheckIfNumbersAreDigitAnagrams(firstNumber, secondNumber);
+
+            // Assert
+            Assert.AreEqual(expected, result);
         }
     }
 }
