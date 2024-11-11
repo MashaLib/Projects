1a7ed9e0ed2dbff1bdca213ad87acbf77cc9ac71
Daria Borisenko <dashapim@gmail.com>
2024-11-11 13:50:42 -0500

    Added tests for Fibonacci

--- LearnLab/LearnLab.UnitTests/MathOperationsTests.cs
+++ LearnLab/LearnLab.UnitTests/MathOperationsTests.cs
@@ -64,5 +64,116 @@ namespace LearnLab.UnitTests
             // Assert
             Assert.AreEqual(expected, result);
         }
+
+        [TestMethod]
+        public void CheckIfNumbersAreFibonacci_BothFibonacciNumbers_ReturnsYesBoth()
+        {
+            //Arrange
+            int firstNumber = 8; //Fibonacci number
+            int secondNumber = 13; //Fibonacci number
+
+            //Act
+            string result = MathOperations.CheckIfNumbersAreFibonacci(firstNumber, secondNumber);
+
+            //Assert
+            Assert.AreEqual("yes, both numbers", result);
+
+        }
+
+        [TestMethod]
+        public void CheckIfNumbersAreFibonacci_OnlyFirstNumberIsFibonacci_ReturnsYesFirstNumber()
+        {
+            //Arrange
+            int firstNumber = 5;    //Fibonacci Number
+            int secondNumber = 4;   //Not a Fibonacci Number
+
+            //Act
+            string result = MathOperations.CheckIfNumbersAreFibonacci(firstNumber, secondNumber);
+
+            //Assert
+            Assert.AreEqual("yes, first number", result);
+
+        }
+
+        [TestMethod]
+
+        public void CheckIfNumbersAreFibonacci_OnlySecondNumberIsFibonacci_ReturnsYesSecondNumber()
+        {
+
+            //Arrange
+            int firstNumber = 4;      //Not a Fibonacci number
+            int secondNumber = 5;     //Fibonacci number
+
+            //Act
+
+            string result = MathOperations.CheckIfNumbersAreFibonacci(firstNumber, secondNumber);
+
+            //Assert
+            Assert.AreEqual("yes, second number", result);
+        }
+
+        [TestMethod]
+
+
+        public void CheckIfNumbersAreFibonacci_NeitherNumberIsFibonacci_ReturnsNo()
+        {
+            //Arrange
+
+            int firstNumber = 4;    //Not a Fibonacci number
+            int secondNumber = 6;    //Not a Fibonacci number
+
+            //Act
+
+            string result = MathOperations.CheckIfNumbersAreFibonacci(firstNumber, secondNumber);
+
+
+            //Assert
+
+            Assert.AreEqual("no", result);
+
+        }
+
+        [TestMethod]
+
+        public void CheckIfNumbersAreFibonacci_NegativeNumbers_ReturnsNo()
+        {
+
+            //Arrange
+
+            int firstNumber = -5;        //Negative number
+
+            int secondNumber = -8;       //Negative number
+
+            //Act
+            string result = MathOperations.CheckIfNumbersAreFibonacci(firstNumber, secondNumber);
+
+            //Assert
+
+            Assert.AreEqual("no", result);
+
+
+        }
+
+
+        [TestMethod]
+
+        public void CheckIfNumbersAreFibonacci_ZeroAsFibonacci_ReturnsYesFirstNumber()
+        {
+            //Arrange
+
+            int firstNumber = 0;      // Fibonacci number
+            int secondNumber = 4;     //Not a Fibonacci number
+
+            //Act 
+            string result = MathOperations.CheckIfNumbersAreFibonacci(firstNumber, secondNumber);
+
+            //Assert
+            Assert.AreEqual("yes, first number", result);
+
+        }
+
     }
+
+
+
 }
