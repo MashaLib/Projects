96b40aad09842e0c231395d9b8e2319d03878fa0
Valeryia <provance.minsk@gmail.com>
2024-11-11 14:51:53 -0500

    Added Test Methods to CalculateDivision Method

--- LearnLab/LearnLab.UnitTests/MathOperationsTests.cs
+++ LearnLab/LearnLab.UnitTests/MathOperationsTests.cs
@@ -65,4 +65,91 @@ namespace LearnLab.UnitTests
             Assert.AreEqual(expected, result);
         }
     }
+    [TestClass]
+    public class CalculateDivisionTests
+    {
+        [TestMethod]
+        public void Test_DivisionWithPositiveNumbers()
+        {
+            int firstNumber = 10;
+            int secondNumber = 2;
+            string expectedResult = "5";
+
+            string result = MathOperations.CalculateDivision(firstNumber, secondNumber);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Test_DivisionWithNegativeAndPositiveNumber()
+        {
+            int firstNumber = -10;
+            int secondNumber = 2;
+            string expectedResult = "-5";
+
+            string result = MathOperations.CalculateDivision(firstNumber, secondNumber);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Test_DivisionWithBothNegativeNumbers()
+        {
+            int firstNumber = -10;
+            int secondNumber = -2;
+            string expectedResult = "5";
+
+            string result = MathOperations.CalculateDivision(firstNumber, secondNumber);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Test_DivisionResultingInDecimal()
+        {
+            int firstNumber = 7;
+            int secondNumber = 2;
+            string expectedResult = "3"; 
+
+            string result = MathOperations.CalculateDivision(firstNumber, secondNumber);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Test_DivisionByZero()
+        {
+            int firstNumber = 10;
+            int secondNumber = 0;
+            string expectedResult = "Division by zero is not allowed";
+
+            string result = MathOperations.CalculateDivision(firstNumber, secondNumber);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Test_ZeroDividedByNumber()
+        {
+            int firstNumber = 0;
+            int secondNumber = 5;
+            string expectedResult = "0";
+
+            string result = MathOperations.CalculateDivision(firstNumber, secondNumber);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Test_DivisionWithOne()
+        {
+            int firstNumber = 10;
+            int secondNumber = 1;
+            string expectedResult = "10";
+
+            string result = MathOperations.CalculateDivision(firstNumber, secondNumber);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
 }
